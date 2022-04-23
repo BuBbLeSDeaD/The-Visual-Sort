38a2e35e76c987ef7bb728ef5335ce1cce92e312
BuBbLeSDeaD <104251433+BuBbLeSDeaD@users.noreply.github.com>
2022-04-23 13:48:15 +0530

    Add files via upload

---
+++ The Visual Sort/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: -40px;
+  width: 100%;
+`
+
+export function Footer() {
+  return (
+    <FooterDiv>
+      Made with Passion by&nbsp;
+      <a style={{ color: '#3f51b5', fontSize: '20px' }}>
+        Team Bhagwadhaari Coders
+      </a>
+    </FooterDiv>
+  )
+}
